ae7af7d298dbd05d8556b97822a77a812710dc8c
Salvador Guzman <guzmansalv@gmail.com>
2018-09-26 23:30:21 -0700

    since size is static - do not print channel returned

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,8 @@ extern crate rand;
 
 mod lib;
 
-
-
 fn main() {
     let chans = lib::channels("root", "", "localhost", 5432, "youtube");
-    println!("Retrieved {} channels", chans.len());
     let chans_priors = lib::prior_adjust(chans);
 
     println!("{}", chans_priors.len());
