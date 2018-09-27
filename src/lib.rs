44dd8932f6cc8612f50b10f35d7bd92ed0852528
Salvador Guzman <guzmansalv@gmail.com>
2018-09-26 20:54:11 -0700

    lib.rs with postgresql funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
 extern crate postgres;
 
-use postgres::{Connection, TlsMode};
-
 pub struct Channel {
     pub channel_serial: String,
     pub id: i64
 }
 
-pub fn connect(user: &str, pass: &str, host: &str, port: u16, db: &str) -> postgres::Connection {
+fn connect(user: &str, pass: &str, host: &str, port: u16, db: &str) -> postgres::Connection {
     let postgres_url = format!("postgres://{}:{}@{}:{}/{}", user, pass, host, port, db);
 
-    let conn = Connection::connect(postgres_url, TlsMode::None)
+    let conn = postgres::Connection::connect(postgres_url, postgres::TlsMode::None)
         .unwrap();
 
     return conn;
+}
+
+pub fn channels(user: &str, pass: &str, host: &str, port: u16, db: &str) -> Vec<Channel> {
+    let conn = connect(user, pass, host, port, db);
+    let query_results = conn.query("SELECT channel_id, id FROM youtube.channels.channels", &[]).unwrap();
+
+    let mut vec = Vec::new();
+    for row in &query_results {
+        let chan = Channel {
+            channel_serial: row.get(0),
+            id: row.get(1)
+        };
+        vec.push(chan);
+    }
+
+    conn.finish().unwrap();
+    return vec;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 mod lib;
 
-
 fn main() {
-    let conn = lib::connect("root", "", "localhost", 5432, "youtube");
+    let chans = lib::channels("root", "", "localhost", 5432, "youtube");
 
-    for row in &conn.query("SELECT channel_id, id FROM youtube.channels.channels", &[]).unwrap() {
-        let chan = lib::Channel {
-            channel_serial: row.get(0),
-            id: row.get(1)
-        };
-        println!("Found person {} {}", chan.id, chan.channel_serial);
-    }
+    println!("Retrieved {} channels", chans.len());
 
-    conn.finish().unwrap()
 }
