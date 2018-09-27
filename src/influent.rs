e098b13300d06470136d5506b6ef872bf03b3093
Salvador Guzman <guzmansalv@gmail.com>
2018-09-27 00:31:40 -0700

    simple connect

---
+++ src/influent.rs
@@ -0,0 +1,23 @@
+extern crate influent;
+
+const USER: &str = "admin";
+const PASS: &str = "admin";
+const HOST: &str = "localhost";
+const PORT: u16 = 8086;
+const DB: &str = "youtube";
+
+pub fn connect() {
+    use influent::create_client;
+    use influent::client::{Client, Credentials};
+    use influent::measurement::{Measurement, Value};
+
+    let credentials = Credentials {
+        username: USER,
+        password: PASS,
+        database: DB
+    };
+
+    let url = format!("http://{}:{}", HOST, PORT);
+    let hosts = vec![url];
+    create_client(credentials, hosts)
+}
